f8be02e0c5b2bab16f321bde56b46dd724db7943
Marcin <marcin.wojtala@oup.com>
2019-08-02 11:10:27 +0100

    webpack3 example added

---
+++ app/webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const distUrl = 'dist';
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './public/index.html',
+  inject: true,
+});
+
+const HotModuleReplacementPluginConfig = new webpack.HotModuleReplacementPlugin();
+
+module.exports = {
+  entry: ['babel-polyfill', 'react-hot-loader/patch', './src/index.js'],
+  output: {
+    path: path.resolve(distUrl),
+    filename: 'bundle.js',
+  },
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: ['react-hot-loader/webpack', 'babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jpe?g$|\.gif$|\.png$|^(?!.*\.inline\.svg$).*\.svg$/,
+        loader: 'url-loader',
+      },
+    ],
+  },
+  plugins: [HtmlWebpackPluginConfig, HotModuleReplacementPluginConfig],
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, distUrl),
+    publicPath: '/',
+    historyApiFallback: true,
+    disableHostCheck: true,
+  },
+};
--- nodemon/index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  console.log('HE ___ O');
-  res.end();
-}).listen(3000);
